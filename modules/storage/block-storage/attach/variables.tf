38d3003afdacc59661fb94a61353318d7a1e633b
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-11-23 17:00:57 -0300

    Create variables.tf

---
+++ modules/storage/block-storage/attach/variables.tf
@@ -0,0 +1,18 @@
+variable "display_name" {
+  type = string
+}
+variable "size_in_gbs" {
+  type = number
+}
+variable "backup_policy_name" {
+  type = string
+}
+variable "client_compartment_name" {
+  type = string
+}
+variable "client_compartment_id" {
+  type = string
+}
+variable "compartment_name" {}
+variable "common_tags" {}
+variable "tag_cost_tracker" {}
