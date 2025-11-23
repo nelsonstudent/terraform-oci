bd3cf67a7e062666832070adcbadcd43fc3781b3
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-11-23 17:02:18 -0300

    Create variables.tf

---
+++ modules/storage/block-storage/variables.tf
@@ -0,0 +1,10 @@
+variable "client_compartment_id" {
+  type = string
+}
+variable "instance_compartment_name" {
+  type = string
+}
+variable "block_volumes" {}
+variable "instance_name" {
+  type = string
+}
