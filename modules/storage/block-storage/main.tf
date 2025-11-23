c5e10e529d920db9994d3015bc5a5e1467b76de1
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-11-23 17:07:52 -0300

    Update main.tf

--- modules/storage/block-storage/main.tf
+++ modules/storage/block-storage/main.tf
@@ -1,37 +1,46 @@
-resource "oci_core_volume_attachment" "attach_block_volume" {
-  for_each = {
-    for vol in var.block_volumes : vol => vol
+locals {
+  module_common_tags = {
+    module = "blockvolume"
   }
+}
 
-  #attachment_type = "iscsi" or "paravirtualized" (pv use to truenas)
-  attachment_type = "paravirtualized"
-  instance_id = element(data.oci_core_instances.list_instances.instances, index(
-    data.oci_core_instances.list_instances.instances[*].display_name, var.instance_name
+resource "oci_core_volume" "block_volume" {
+  compartment_id = element(data.oci_identity_compartments.list_compartments.compartments, index(
+    data.oci_identity_compartments.list_compartments.compartments[*].name, var.compartment_name
   )).id
-  volume_id = element(data.oci_core_volumes.list_volumes.volumes, index(
-    data.oci_core_volumes.list_volumes.volumes[*].display_name, each.key
-  )).id
-  display_name = "${var.instance_name}-${each.key}"
+  display_name          = var.display_name
+  availability_domain   = data.oci_identity_availability_domains.list_availability_domains.availability_domains[0].name # Usa sempre o primeiro AD
+  size_in_gbs           = var.size_in_gbs
+  is_auto_tune_enabled  = true
+  defined_tags          = { "${var.tag_cost_tracker}" = var.client_compartment_name }
+  freeform_tags         = merge(var.common_tags, local.module_common_tags)
 
   lifecycle {
     ignore_changes = [
-      instance_id,
-      volume_id
+      defined_tags["Oracle-Tags.CreatedBy"],
+      defined_tags["Oracle-Tags.CreatedDate"],
+      defined_tags["Oracle-Tags.CreatedOn"],
+      defined_tags["ResouceInfo.Date"],
+      defined_tags["ResouceInfo.Principal"]
     ]
   }
 }
 
-data "oci_core_instances" "list_instances" {
-  compartment_id = element(data.oci_identity_compartments.list_compartments.compartments, index(
-    data.oci_identity_compartments.list_compartments.compartments[*].name, var.instance_compartment_name
-  )).id
+resource "oci_core_volume_backup_policy_assignment" "block_volume_backup_policy_assignment" {
+  depends_on = [oci_core_volume.block_volume]
+  count      = var.backup_policy_name != "" ? 1 : 0
+
+  asset_id = oci_core_volume.block_volume.id
+  policy_id = length(regexall("gold|silver|bronze", var.backup_policy_name)) > 0 ? element(data.oci_core_volume_backup_policies.list_volume_backup_policies.volume_backup_policies, index(
+    data.oci_core_volume_backup_policies.list_volume_backup_policies.volume_backup_policies[*].display_name, var.backup_policy_name
+  )).id : var.backup_policy_name
 }
 
-data "oci_core_volumes" "list_volumes" {
-  compartment_id = element(data.oci_identity_compartments.list_compartments.compartments, index(
-    data.oci_identity_compartments.list_compartments.compartments[*].name, var.instance_compartment_name
-  )).id
-  state = "AVAILABLE"
+data "oci_core_volume_backup_policies" "list_volume_backup_policies" {
+}
+
+data "oci_identity_availability_domains" "list_availability_domains" {
+  compartment_id = var.client_compartment_id
 }
 
 data "oci_identity_compartments" "list_compartments" {
