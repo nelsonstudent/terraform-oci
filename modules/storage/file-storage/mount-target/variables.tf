211508b2d2fa37537848f5f937bbe6f6951f1176
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-11-23 17:17:02 -0300

    Create variables.tf

---
+++ modules/storage/file-storage/mount-target/variables.tf
@@ -0,0 +1,12 @@
+variable "file_storage_system" {}
+variable "compartment_name" {
+  type = string
+}
+variable "compartment_id" {
+  type = string
+}
+variable "common_tags" {}
+variable "tag_cost_tracker" {}
+variable "vcn_id" {
+  type = string
+}
